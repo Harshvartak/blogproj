7c1af3af31ff13334dc5e3f32ae2b799677970be
Harsh Vartak <vartak.harsh@gmail.com>
2019-12-18 22:34:47 +0530

    Started crud procedure in DRF

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -37,7 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #installed apps
+    'post.apps.PostConfig',
+
+
+    #3rd party apps
+    'rest_framework',
+
+
+
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
